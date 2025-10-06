4cbbeb9002b2228a8687f8425e9bada8fa51f034
RPV <rpv@senai.br>
2025-10-06 19:25:05 -0300

    Configuracoes iniciais do servidor

---
+++ server.js
@@ -0,0 +1,18 @@
+import cors from '@fastify/cors';
+import { app } from "./app.js";
+async function server() {
+    app.register(cors, {
+        origin: true,
+        method: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    })
+
+    app.listen({
+        host: '0.0.0.0',
+        port: 3333,
+    }).then(() => {
+        console.log('HTTP is running on PORT 3333 ')
+    })
+}
+
+server();
